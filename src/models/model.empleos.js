2bdb3274e0d3da6462d3ceba7e9d814dcebaf03b
Esteban Olivares <psicologoclinicoesteban@gmail.com>
2024-08-18 13:42:41 -0400

    agregando insert empleo

--- src/models/model.empleos.js
+++ src/models/model.empleos.js
@@ -19,3 +19,14 @@ export const empleoById = async (id) => {
         return error
     }
 }
+
+export const insertEmpleo = async (data) => {
+    const { id, cargo, descripcion, ubicacion, modalidad, salario, nombreEmpresa, creadoEn, listaId, usuarioId } = data
+    try {
+        const consulta ='INSERT INTO empleos (id, cargo, descripcion, ubicacion, modalidad, salario, nombre_empresa, creado_en, lista_id, usuario_id);'
+        const values = [id, cargo, descripcion, ubicacion, modalidad, salario, nombreEmpresa, creadoEn, listaId, usuarioId]
+       return await data(consulta, values)
+    } catch (error) {
+        return error
+    }
+}
