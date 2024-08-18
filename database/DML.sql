aa9ef119f95de1d1014af70ff1e711c52b0ce0da
Esteban Olivares <psicologoclinicoesteban@gmail.com>
2024-08-18 14:02:28 -0400

    comenzando a crear aplicaciones models

--- database/DML.sql
+++ database/DML.sql
@@ -8,3 +8,6 @@ ADD CONSTRAINT roles_id_check CHECK (id IN (1, 2));
 -- Añade una restricción para evitar duplicación de nombres
 ALTER TABLE roles
 ADD CONSTRAINT roles_nombre_unique UNIQUE (nombre);
+
+INSERT INTO listas (id, nombre) VALUES (1, 'deseos'), (2, 'postulado'), (3, 'oferta'), (4, 'rechazado');
+
