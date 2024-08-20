1d96ac534d06c21ac853b24e6e5fcf1780506146
alevllgs <ale.villegas@uc.cl>
2024-08-19 23:13:31 -0400

    corrige config.js para trabajar con ES Modules se hace prueba de grabado en pg

--- database/config.js
+++ database/config.js
@@ -7,7 +7,7 @@ dotenv.config();
 export const pool = new Pool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
-    password: process.env.DB_PASS,
+    password: process.env.DB_PASSWORD, 
     database: process.env.DB_DATABASE,
     port: process.env.DB_PORT,
     allowExitOnIdle: true,
