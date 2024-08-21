3d692da555c916267b99107a89d76c1296b23717
Esteban Olivares <psicologoclinicoesteban@gmail.com>
2024-08-20 21:30:32 -0400

    definiendo middleware de autentificación

--- src/routes/model.routes.js
+++ src/routes/model.routes.js
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { fetchAndSaveCountries } from '../controllers/model.controller.js';
+import { authMiddlewareAdmin, authMiddleware } from '../middleware/middleware_verify.js'
 
 const router = Router();
 
