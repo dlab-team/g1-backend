f9e528710fd9c8c9eb164339a6ab0ccba1fee5f8
Daniel Ferrada <daniel.ferradabarria@gmail.com>
2024-08-22 21:05:19 -0400

    add: models de educacion

--- src/models/educacion.models.js
+++ src/models/educacion.models.js
@@ -1,2 +1,12 @@
 import { pool } from "../../database/config";
 
+// Obtener Educacion
+const callEducacion = async () =>{
+
+    const query = `SELECT * FROM educacion`;
+    const result = await pool.query(query);
+    return result.rows;
+
+}
+
+export default {callEducacion}
