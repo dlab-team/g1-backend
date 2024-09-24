3ad245f7bf329f4733834b8f56f8515690cf50d3
alevllgs <ale.villegas@uc.cl>
2024-09-23 23:04:25 -0300

    incorpora swagger

--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
 import express from 'express';
 import chalk from 'chalk';
-// import rutas from './rutas/rutas.js';
-
-import router from './routes/model.routes.js'
 import cors from 'cors';
+import swaggerRouter from './swagger.js'; // Importar las rutas de Swagger
+import router from './routes/model.routes.js'; // Importar las rutas principales
 
 const app = express();
 
 app.use(express.json());
-app.use(cors())
-app.use("/", router)
+app.use(cors());
 
+// Integrar Swagger en /api-docs
+app.use(swaggerRouter);
+
+// Usar las rutas principales de la aplicación
+app.use("/", router);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(chalk.blue('        🔥  Server On 🔥'));
   console.log(chalk.grey(`Click to open: ${chalk.underline(`http://localhost:${PORT}`)}`));
 });
-
