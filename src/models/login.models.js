36d94b2518a4629b3e13ee81652bccc327ef62d4
Esteban Olivares <psicologoclinicoesteban@gmail.com>
2024-09-29 20:48:24 -0300

    habilitando entrega de rol en login

--- src/models/login.models.js
+++ src/models/login.models.js
@@ -8,17 +8,17 @@ dotenv.config()
 const { JWT_SECRET } = process.env
 
 export const verificarCredenciales = async ({ email, password }) => {
-  const consulta = 'SELECT id, password FROM usuarios WHERE correo = $1;'
+  const consulta = 'SELECT id, rol_id, password FROM usuarios WHERE correo = $1;'
   const values = [email]
   const usuario = await data(consulta, values)
-  const { id } = usuario[0]
+  const { id, rol_id: rol } = usuario[0]
   if (usuario.length === 0) {
     throw new Error('Email o contraseña incorrecta')
   } else {
     const { password: passwordEncriptada } = usuario[0]
     const passwordEsCorrecta = bcrypt.compareSync(password, passwordEncriptada)
     if (passwordEsCorrecta) {
-      return { token: jwt.sign(email, JWT_SECRET), id }
+      return { token: jwt.sign(email, JWT_SECRET), id, rol }
     } else throw new Error('Email o contraseña incorrecta')
   }
 }
