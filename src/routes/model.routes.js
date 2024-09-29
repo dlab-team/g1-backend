581cc8305959480f7696899a250c6765e39b2d20
Esteban Olivares <psicologoclinicoesteban@gmail.com>
2024-09-29 16:04:25 -0300

    arreglando router

--- src/routes/model.routes.js
+++ src/routes/model.routes.js
@@ -1,9 +1,9 @@
-// import { fetchAndSaveCountries } from '../controllers/model.controller.js'
+import { Router } from 'express'
 import { autorizacionUsuario, autorizacionAdmin } from '../middleware/middleware_verify.js'
 import * as empleos from '../controllers/empleos.controller.js'
 import * as actividades from '../controllers/actividades.controller.js'
 import { credenciales } from '../controllers/login.controller.js'
-import { usuariosLog } from '../middleware/usuarios.middelware.js';
+import { usuariosLog } from '../middleware/usuarios.middelware.js'
 import {
   agregarUsuario,
   verUsuario,
